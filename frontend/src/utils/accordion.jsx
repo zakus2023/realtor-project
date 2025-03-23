c54bc70ea875e361ae2cc4ff237d194d48d29c7b
abdul <idbsch2012@gmail.com>
2025-03-23 14:47:58 -0400

    updated environment variables

--- frontend/src/utils/accordion.jsx
+++ frontend/src/utils/accordion.jsx
@@ -5,19 +5,19 @@ const data = [
     icon: <HiShieldCheck />,
     heading: "Best interest rates",
     description:
-      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      "Take advantage of our special promotion with reduced interest rates for a limited time! Whether you're looking to invest, refinance, or take out a new loan, this is the perfect opportunity to save more. Don't miss out—secure lower rates today and maximize your benefits before the offer ends!",
   },
   {
     icon: <MdCancel />,
     heading: "Prevent unstable prices",
     description:
-      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      "To avoid high costs, consider buying in bulk, comparing prices from different realtors, and taking advantage of discounts and promotions. Plan purchases ahead, negotiate when possible, and opt for quality properties that offer long-term value. Staying informed about market trends can also help you make cost-effective decisions and prevent overspending.",
   },
   {
     icon: <MdAnalytics />,
     heading: "Best price on the market",
     description:
-      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      "Looking for the best deal? We offer competitive prices without compromising on quality. Compare, save, and enjoy top-notch properties/services at the most affordable rates. Don't miss out—grab the best price on the market today! 🚀",
   },
 ];
 export default data;

0ffbae5f6ae21e344902b2805f25bbf6dd89d857
abdul <idbsch2012@gmail.com>
2025-03-23 23:57:33 -0400

    fixed frontend deploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root")).render(
       domain="dev-ccmy6um2itf0jid5.us.auth0.com"
       clientId="lHlNWSq1xLOkWXpViqAnLCjmT0hppfqq"
       authorizationParams={{
-        redirect_uri: "http://localhost:5173",
+        redirect_uri: "https://aethersoft-frontend.onrender.com/callback",
       }}
       audience="http://localhost:5000"
       scope="openid profile email"

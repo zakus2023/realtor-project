226ce8b8ed5867f7ac7523e16f3459ff56739d85
abdul <idbsch2012@gmail.com>
2025-03-23 20:38:15 -0400

    fixed frontend deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    outDir: 'dist', // Must match this exactly
+    emptyOutDir: true
+  }
 })

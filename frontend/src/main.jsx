5540cb9132ebbddf6b83fc5a86e09a304bab47cf
abdul <idbsch2012@gmail.com>
2025-03-24 00:38:20 -0400

    fixed frontend deploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,15 +7,15 @@ import { Auth0Provider } from "@auth0/auth0-react";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Auth0Provider
-      domain="dev-ccmy6um2itf0jid5.us.auth0.com"
-      clientId="7rwwJVBGjwYR4jdd8CG3E6wlnxcdWaPC"
-      authorizationParams={{
-        redirect_uri: "https://aethersoft-frontend.onrender.com/callback",
-      }}
-      audience="http://localhost:5000"
-      scope="openid profile email"
-    >
-      <App />
-    </Auth0Provider>
+  domain="dev-ccmy6um2itf0jid5.us.auth0.com"
+  clientId="7rwwJVBGjwYR4jdd8CG3E6wlnxcdWaPC"
+  authorizationParams={{
+    redirect_uri: window.location.origin + "/callback", // Auto-adapts to local/prod
+    audience: "http://localhost:5000", // Must match Auth0 API identifier
+    scope: "openid profile email",
+  }}
+>
+  <App />
+</Auth0Provider>
   </StrictMode>
 );
